eb8f53244e46862336e42e609e11db0cfa37ef87
fancy22donga <155005642+fancy22donga@users.noreply.github.com>
2024-02-28 08:26:18 +0530

    DATABASE

--- input.php
+++ input.php
@@ -1,26 +1,22 @@
 <html>
 <body>
 <form action="output.php" method="get">
+enter username:<input type="text" name="uname" />
+<br/>
+enter password:<input type="text" name="pw" />
+<br/>
 enter firstname:<input type="text" name="my_firstname" />
 <br/>
 enter lastname:<input type="text" name="my_lastname" />
 <br/>
-enter class:<input type="text" name="class" />
+enter email:<input type="text" name="email" />
 <br/>
-enter division:<input type="text" name="div" />
+enter Mobile no.:<input type="text" name="mno" />
 <br/>
-enter city:<input type="text" name="my_city" />
-<br/>
+
 enter address:<input type="text" name="add" />
 <br/>
-enter mobile no:<input type="text" name="mob_no" />
-<br/>
-enter pincode:<input type="text" name="pin" />
-<br/>
-enter state:<input type="text" name="my_state" />
-<br/>
-enter country:<input type="text" name="my_country" />
-<br/>
+
 
 <input type="submit" name="submit" >
 </form>
--- output.php
+++ output.php
@@ -1,25 +1,20 @@
 <?php
+$uname=$_REQUEST['uname'];
+$pw=$_REQUEST['pw'];
 $firstname=$_REQUEST['my_firstname'];
 $lastname=$_REQUEST['my_lastname'];
-$class=$_REQUEST['class'];
-$division=$_REQUEST['div'];
-$city=$_REQUEST['my_city'];
+$email=$_REQUEST['email'];
+$mno=$_REQUEST['mno'];
 $address=$_REQUEST['add'];
-$mobile_no=$_REQUEST['mob_no'];
-$pincode=$_REQUEST['pin'];
-$state=$_REQUEST['my_state'];
-$country=$_REQUEST['my_country']; 
 
 echo "<table border=1 >
+      <tr><td>username:</td><td>$uname</td></tr>
+      <tr><td>password:</td><td>$pw</td></tr> 
       <tr><td>first name:</td><td>$firstname</td></tr>
       <tr><td>last name:</td><td>$lastname</td></tr>
-	  <tr><td>class:</td><td>$class</td></tr>
-	  <tr><td>division<td>$division></td></tr>
-	  <tr><td>city:</td><td>$city</td></tr>
-	  <tr><td>address:</td><td>$address</td></tr>
-	  <tr><td>mobile no:</td><td>$mobile_no</td></tr>
-	  <tr><td>pincode:</td><td>$pincode</td></tr>
-	  <tr><td>state:</td><td>$state</td></tr>
-	  <tr><td>country:</td><td>$country</td></tr>
+	   <tr><td>email:</td><td>$email</td></tr>
+	  <tr><td>Mobile no.:</td><td>$mno</td></tr> 
+	 <tr><td>address:</td><td>$address</td></tr>
+	  
 	  </table>";
 ?>
